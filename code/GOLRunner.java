561303337eefac16d032e5453159e32990031cb4
Edward <81455653+foqsi@users.noreply.github.com>
2023-01-03 21:56:57 -0600

    Update GOLRunner.java

--- code/GOLRunner.java
+++ code/GOLRunner.java
@@ -1,6 +1,3 @@
-/**
- * Description: Main method class for Conway's game of life.
- */
 public class GOLRunner {
     public static void main(String[] args) {
         if (args.length < 1) {
@@ -10,9 +7,7 @@ public class GOLRunner {
             int i = 0;
             do {
                 System.out.print(automaton);
-                // evolve the automaton, using GOL.evolve();
                 automaton.evolve();
-                // pause for a milliseconds, using GOL.pause(100);
                 automaton.pause(100);
                 i++;
             } while (i < 500);
