ba7d57bde2000406d61936eecfda7f89b347f512
Edward <81455653+foqsi@users.noreply.github.com>
2023-01-03 21:56:18 -0600

    update GOL.java

--- code/GOL.java
+++ code/GOL.java
@@ -1,22 +1,14 @@
-/**
- * File: GOL.java
- * Description: Conway's game of life.
- */
-
 public class GOL extends Board {
     final char ALIVE = '*';
     final char DEAD = ' ';
 
     public GOL(String filepath) {
-        // call the parent's (Board) constructor and passes path.
         super(filepath);
     }
 
     public void evolve() {
         char[][] nextGen = this.getCopy();
-        // two dimensional loop that iterates over this.cells
-        // and reassigns each state in nextGen according to the
-        // current cell's neighbours and game of life's rules. 
+        
         int rows = this.cells.length, columns = this.cells[0].length;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
@@ -42,8 +34,6 @@ public class GOL extends Board {
     private int countAliveNeighbours(int i, int j) {
         int count = 0;
         int rows = this.cells.length, columns = this.cells[0].length;
-        // for a given cell i, j, return how many of its neighbours
-        // are alive. 
         int rowMin = Math.max(i - 1, 0);
         int rowMax = Math.min(i + 1, rows - 1);
         int colMin = Math.max(j - 1, 0);
@@ -60,8 +50,6 @@ public class GOL extends Board {
     }
 
     private boolean isAlive(int i, int j) {
-        // determine whether a cell is alive by comparing it's
-        // character with this.ALIVE.
         try {
             return this.ALIVE == this.cells[i][j];
         } catch (IndexOutOfBoundsException ex) {
